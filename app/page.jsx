03e9c67e7029cd602e4675b8a480b4429a4e0a29
Raheem Kawojue <kawojue08@gmail.com>
2023-05-13 21:03:17 +0100

    refractored

--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,15 @@ import Feed from '@components/Feed'
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">
-            Soul Speaks
-            <br className="max-md:hidden" />
-            <span className="blue_gradient">Confess, Reflect, Grow</span>
-        </h1>
-        <p className="desc text-center">
-        Experience the liberation of sharing your daily confessions. Connect with a community of like-minded individuals on a journey of personal growth, finding strength in vulnerability. Embrace the power of authenticity and discover the path to empowered living.        </p>
-        <Feed/>
+      <h1 className="head_text text-center">
+        Soul Speaks
+        <br className="max-md:hidden" />
+        <span className="blue_gradient">Confess, Reflect, Grow</span>
+      </h1>
+      <p className="desc text-center">
+        Experience the liberation of sharing your daily confessions. Connect with a community of like-minded individuals on a journey of personal growth, finding strength in vulnerability. Embrace the power of authenticity and discover the path to empowered living.
+      </p>
+      <Feed />
     </section>
   )
 }
