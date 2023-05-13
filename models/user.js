e525a3ef653f771f3e5a1b8e1b47234fa0e3f9cc
Raheem Kawojue <kawojue08@gmail.com>
2023-05-13 20:57:20 +0100

    refractored

--- models/user.js
+++ models/user.js
@@ -1,23 +1,17 @@
 import { Schema, model, models } from "mongoose";
 
-
 const userSchema = new Schema ({
     email : {
         type: String,
         unique: [true, 'Email Exists'],
         required: [true, 'Email Required']
     },
-    
     username: {
         type: String,
         required: [true, 'Username is required!'],
         match: [/^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
     },
-    image: {
-        type: String,
-    }
+    image: String,
 })
 
-const User = models.User || model("User", userSchema);
-
-export default User;
+export default models.User || model("User", userSchema);
