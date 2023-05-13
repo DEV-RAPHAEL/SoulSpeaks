757fd559f570aa0d50d844455fcee73d4da64dcc
Raheem Kawojue <kawojue08@gmail.com>
2023-05-13 21:02:12 +0100

    [optimize] copy func

--- components/card.jsx
+++ components/card.jsx
@@ -5,16 +5,21 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 
-const Card = ({post, handleTagClick, handleEdit, handleDelete}) => {
+const Card = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
 
-  const handleCopy = () =>{
-    setCopied(post.declaration);
-    navigator.clipboard.writeText(post.declaration);
-    setTimeout(() => setCopied(""), 3000);
+  const handleCopy = async () => {
+    try {
+      setCopied(post.declaration);
+      await navigator.clipboard.writeText(post.declaration);
+      setTimeout(() => setCopied(""), 3000);
+    } catch {
+      setCopied("Failed to copy!")
+    }
   }
-  return(
-      <div className="card">
+
+  return (
+    <div className="card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-2 flex justify-start items-center gap-3 cursor-pointer">
           <Image
@@ -23,29 +28,26 @@ const Card = ({post, handleTagClick, handleEdit, handleDelete}) => {
             width={40}
             height={40}
             className="rounded-full object-contain" />
-        <div className="flex flex-col">
-          <h3 className="font-satoshi font-semibold text-grey-900">{post.creator.username}</h3>
-          <h3 className="font-inter text-sm text0grey-500">{post.creator.email}</h3>
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-grey-900">{post.creator.username}</h3>
+            <h3 className="font-inter text-sm text0grey-500">{post.creator.email}</h3>
+          </div>
         </div>
-      </div>
-      <div className="copy_btn" onClick={handleCopy}>
-        <Image
-          src={copied === post.declaration
-            ? '/assets/icons/tick.svg'
-            : '/assets/icons/copy.svg'}
-          width={12}
-          height={14} />
-      </div>
+        <div className="copy_btn" onClick={async () => await handleCopy()}>
+          <Image
+            src={copied === post.declaration
+              ? '/assets/icons/tick.svg'
+              : '/assets/icons/copy.svg'}
+            width={12}
+            height={14} />
+        </div>
       </div>
       <p className="my-4 font-satoshi text-sm">{post.declaration}</p>
       <p className="font-inter text-sm blue_gradient cursor-pointer"
-        onClick={() => handleTagClick && handleTagClick(post.tag)}
-      >
+        onClick={() => handleTagClick && handleTagClick(post.tag)}>
         {post.tag}
       </p>
-     
-      </div>
-      
+    </div>
   )
 };
 
