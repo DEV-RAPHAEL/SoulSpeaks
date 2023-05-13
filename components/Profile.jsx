d1613885c72e857f239e6b4ae038d6c5b945c51a
Raheem Kawojue <kawojue08@gmail.com>
2023-05-13 20:57:57 +0100

    refractored

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -8,16 +8,16 @@ const Profile = ({
     <section className="w-full">
       <h1 className="head_text text-left">
         <span className="blue_gradient">
-        {name} Profile  </span>
-        </h1>
-        <p className="desc text-left">
-          {desc}
-        </p>
-{/*         <div className="mt-16 card_layout">
-         {data.map((post) => (
-        <Card key={post._id} 
-        post={post} 
-         />
+          {name} Profile  </span>
+      </h1>
+      <p className="desc text-left">
+        {desc}
+      </p>
+      {/*         <div className="mt-16 card_layout">
+          {data.map((post) => (
+          <Card key={post._id} 
+          post={post} 
+          />
       ))}
     </div> */}
 
