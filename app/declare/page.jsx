8244bb5052a8c89125abe558e108744cbbffdd57
Raheem Kawojue <kawojue08@gmail.com>
2023-05-13 21:04:27 +0100

    [optimized] fetching

--- app/declare/page.jsx
+++ app/declare/page.jsx
@@ -1,50 +1,43 @@
 "use client"
+import axios from 'axios'
 import { useState } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import Form from "@components/Form";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 const Declare = () => {
   const router = useRouter();
-  const {data : session } = useSession();
+  const { data: session } = useSession();
 
   const [submitting, setSubmitting] = useState(false)
-  const [post, setPost] = useState({
-      declaration: '',
-      tag : ''
-});
+  const [post, setPost] = useState({ declaration: '', tag: '' });
 
-const declare = async (e) => {
+  const declare = async (e) => {
     e.preventDefault();
     setSubmitting(true);
-try {
-    const response = await fetch('/api/declarations/new', {
-      method : 'POST',
-      body: JSON.stringify({
+    await axios.post(
+      '/api/declarations/new',
+      JSON.stringify({
         declaration: post.declaration,
-        userId : session?.user.id,
-        tag : post.tag
+        userId: session?.user.id,
+        tag: post.tag
       })
-    })
-    if (response.ok){
+    ).then(res => {
       router.push('/')
-    }
-} catch (error) {
-    console.log(error)
-}
-  finally{
-    setSubmitting(false)
+    }).catch(err => {
+      console.log(err)
+    }).finally(() => {
+      setSubmitting(false)
+    })
   }
 
-}
   return (
     <Form
-    type="Declare"
-    post={post}
-    setPost={setPost}
-    submitting={submitting}
-    handleSubmit={declare}
-    />
+      type="Declare"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={declare} />
   )
 }
 
