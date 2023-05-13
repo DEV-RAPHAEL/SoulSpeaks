c22faafb6fc6218dca7d9981fdbf4d7b734be1d5
Raheem Kawojue <kawojue08@gmail.com>
2023-05-13 21:03:58 +0100

    [optimized] fetching

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,36 +1,57 @@
 "use client"
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import Profile from '../../components/Profile';
 import { useSession } from "next-auth/react";
 
 const MyProfile = () => {
-    const { data : session} = useSession();
+  const [posts, setPosts] = useState([])
+  const { data: session } = useSession()
 
-    const [posts, setPosts] = useState([])
-    useEffect(() => {
-        const fetchPosts = async () => {
-          const response = await fetch(`/api/users/${session?.user.id}/posts`);
-          const data = await response.json();
-          setPosts(data);
-        }
-        if(session?.user.id)fetchPosts();
-      }, []);
-
-    const handleEdit = () => {
-
+  useEffect(() => {
+    const fetchPosts = async () => {
+      await axios.get(`/api/users/${session?.user.id}/posts`)
+        .then(res => {
+          setPosts(res.data);
+        }).catch(err => {
+          console.log(err.code)
+        })
     }
-    const handleDelete = async => {
 
-    }
+    if (session?.user.id) (async () => await fetchPosts())();
+  }, []);
+
+  const handleEdit = async (id) => {
+    await axios.put(``, JSON.stringify({}))
+      .then(res => {
+        // resolved
+        console.log("Edited")
+      }).catch(err => {
+        // rejected
+        console.log(err.code)
+      })
+  }
+
+  const handleDelete = async (id) => {
+    await axios.delete(``, JSON.stringify({}))
+      .then(res => {
+        // resolved
+        console.log("Deleted")
+      }).catch(err => {
+        // rejected
+        console.log(err.code)
+      })
+  }
+
   return (
-   <Profile 
-        name='My'
-        desc="Welcome to my personal Profile"
-        data={[posts]}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
-   />
+    <Profile
+      name='My'
+      desc="Welcome to my personal Profile"
+      data={[posts]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
 
