34c628482b853521330414a7372844337ba469a6
Raheem Kawojue <kawojue08@gmail.com>
2023-05-14 17:15:08 +0100

    .

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -4,14 +4,12 @@ import Declaration from "@models/declaration";
 export const GET = async (request, { params } ) => {
     try {
         await connectToDB();
-
-        
         const declarations = await Declaration.find({
             creator: params.id
         }).populate('creator');
 
         return new Response(JSON.stringify(declarations), {
-            status : 200 
+            status : 200
         })
     } catch (error) {
         return new Response("Failed to Fetch Declarations", { status : 500 })   
