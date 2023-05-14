57bf4082a3088118658ea371e13f16ac803132ec
Raheem Kawojue <kawojue08@gmail.com>
2023-05-14 22:32:08 +0100

    [fetch] cards

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -2,11 +2,14 @@ import Card from "./card";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
 
+  console.log(data)
+
   return (
     <section className="w-full">
       <h1 className="head_text text-left">
         <span className="blue_gradient">
-          {name} Profile  </span>
+          {name} Profile
+        </span>
       </h1>
       <p className="desc text-left">
         {desc}
