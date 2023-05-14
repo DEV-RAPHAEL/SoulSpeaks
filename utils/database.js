203696e2ee2d58385e4636c46dd534c024a7370f
Raheem Kawojue <kawojue08@gmail.com>
2023-05-14 16:39:19 +0100

    ,

--- utils/database.js
+++ utils/database.js
@@ -1,22 +1,8 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
-
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
-
-    if (isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
-
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "soulspeaks",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        isConnected = true;
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to MongoDB!")
     } catch (error) {
         console.log(error)
