a66ce1b78eac2ada88bffe6cf25e5e6a3a39362d
Raheem Kawojue <kawojue08@gmail.com>
2023-05-16 09:00:10 +0100

    .

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectToDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "SoulSpeaks"
+        })
         console.log("Connected to MongoDB!")
     } catch (error) {
         console.log(error)
